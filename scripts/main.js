6cc13f297ffecdfddb644d5e8e392d0165737a65
talrez11 <tal@way2cu.com>
2014-12-15 17:36:42 +0200

    added main.js scripts

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,12 @@
  * Copyright (c) 2014. by Way2CU, http://way2cu.com
  * Authors:
  */
-
+ var Caracal = Caracal || {};
+ 
 function on_site_load() {
+
+	// create image lightbox
+	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
 }
 
 $(on_site_load);
