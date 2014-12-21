fc5bfe7e3e85c69a9c6e80406edd03b6b24cdb24
talrez11 <tal@way2cu.com>
2014-12-21 14:28:19 +0200

    Added Fix script main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,15 +14,28 @@ function on_site_load() {
 	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
     
 	// map Canvas
+	if ($('div#map_canvas').length) {
 	var image = "/site/images/map-pin.png";
 	var mapOptions = {
 	  zoom: 17,
 	  center: new google.maps.LatLng(31.870843, 34.804183), 
-	  mapTypeId: google.maps.MapTypeId.ROADMAP 
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
 	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 	var myPos = new google.maps.LatLng(31.870843, 34.804183); 
 	var myMarker = new google.maps.Marker({position: myPos, map: map, icon: image });
+	}
+
+	// Testimonial 
+	if ($('div.testimonial').length > 0) {
+
+		Caracal.testimonial_pages = new PageControl('div.testimonials_container', 'div.testimonial')
+		Caracal.testimonial_pages.attachNextControl($('a.arrow.next'))
+		Caracal.testimonial_pages.attachPreviousControl($('a.arrow.previous'))
+		.setInterval(10000)
+		.setWrapAround(true)
+		.setPauseOnHover(true);
+	}
 }
 
 $(on_site_load);
